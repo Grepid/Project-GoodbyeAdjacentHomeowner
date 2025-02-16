86986cee72e5941c4df7525c5ac3534e8059b8df
ELLIS WOODS <ew280189@falmouth.ac.uk>
2025-02-16 15:44:46 +0000

    Generic Interactions script made

--- Assets/Scripts/Generics/GenericActions.cs
+++ Assets/Scripts/Generics/GenericActions.cs
@@ -5,10 +5,39 @@ using UnityEngine.Events;
 
 public class GenericActions : InteractionBase
 {
+    public bool Repeats
+    {
+        get
+        {
+            return repeats;
+        }
+        set
+        {
+            repeats = value;
+            if (value) hasReachedEnd = false;
+        }
+    }
+    public bool repeats;
     [SerializeField]
-    private UnityEvent InteractEvent;
+    private UnityEvent[] InteractEvents;
+    bool hasReachedEnd;
+
+    int iCount;
     protected override void OnInteract()
     {
-        InteractEvent?.Invoke();   
+        if (iCount == 0 && !repeats && hasReachedEnd) return;
+        InteractEvents[iCount]?.Invoke();
+        iCount++;
+        if(iCount == InteractEvents.Length)
+        {
+            iCount = 0;
+            hasReachedEnd = true;
+        }
+    }
+
+    [ContextMenu("FlipRepeats")]
+    private void FlipRepeats()
+    {
+        Repeats = !Repeats;
     }
 }
