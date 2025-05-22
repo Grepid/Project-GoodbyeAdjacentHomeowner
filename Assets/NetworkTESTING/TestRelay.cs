b6b7ce8c548c701d54775dc62dfc8c26918f5c83
ELLIS WOODS <ew280189@falmouth.ac.uk>
2025-05-22 22:09:44 +0100

    Relay Connection works + imported Lobby From Codemonkey

--- Assets/NetworkTESTING/TestRelay.cs
+++ Assets/NetworkTESTING/TestRelay.cs
@@ -1,17 +1,64 @@
+using QFSW.QC;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 
 public class TestRelay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private async void Start()
     {
+        await UnityServices.InitializeAsync();
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
-    // Update is called once per frame
-    void Update()
+    [Command]
+    private async void CreateRelay()
     {
-        
+        try
+        {
+            Allocation alloc = await RelayService.Instance.CreateAllocationAsync(3);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(alloc.AllocationId);
+
+
+
+            RelayServerData rsd = new RelayServerData(alloc, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(rsd);
+            NetworkManager.Singleton.StartHost();
+
+            print($"Relay Join Code: {joinCode}");
+        }
+        catch(RelayServiceException e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+
+
+    [Command]
+    private async void JoinRelay(string joinCode)
+    {
+        try
+        {
+            print($"Joining Relay with code {joinCode}");
+            JoinAllocation ja = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            RelayServerData rsd = new RelayServerData(ja, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(rsd);
+            NetworkManager.Singleton.StartClient();
+            print($"Successfully Joined Relay {joinCode}");
+        }
+        catch(RelayServiceException e)
+        {
+            Debug.LogException(e);
+        }
     }
 }
